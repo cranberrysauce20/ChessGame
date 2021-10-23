b90fbcee9597c5deaa92de081cc5cfade95e1ed5
PAULEENE ADIREA JORDAN <paj81@scarletmail.rutgers.edu>
2021-10-23 18:04:34 +0000

    Piece.java created online with Bitbucket

---
+++ Piece.java
@@ -0,0 +1,36 @@
+package src;
+
+public abstract class Piece {
+    private boolean whiteTurn = false;
+    private boolean remove = false;
+    public boolean first = true;
+    
+    public Piece(boolean whiteTurn)
+    {
+        this.setWhiteTurn(whiteTurn);
+    }
+
+    public boolean isWhite()
+    {
+        return this.whiteTurn;
+    }
+    
+    public boolean removePiece()
+    {
+        return this.remove;
+    }
+
+    public void setWhiteTurn(boolean whiteTurn)
+    {
+        this.whiteTurn = whiteTurn;
+    }
+
+    public void setremovePiece(boolean remove)
+    {
+        this.remove = remove;
+    }
+    
+    public abstract String toString();
+    
+    public abstract boolean allowedMove(int prevX, int prevY, int newX, int newY, boolean isEmpty);
+}
