2051ee1ede81e94be7b93775cbc8c1dbbba90c53
PAULEENE ADIREA JORDAN <paj81@scarletmail.rutgers.edu>
2021-10-23 17:42:26 +0000

    Bishop.java created online with Bitbucket

---
+++ Bishop.java
@@ -0,0 +1,40 @@
+package Source;
+
+public class Bishop extends Piece{
+    private String chessPiece;
+
+    public Bishop(boolean whiteTurn) {
+        super(whiteTurn);
+    
+            this.setWhiteTurn(whiteTurn);
+            //check if white piece is playing or black
+            if(whiteTurn == true)
+            {
+                this.chessPiece = "wB";
+            }
+            else{
+                this.chessPiece = "bB";
+            }
+        
+    }
+// checks if King is allowed to move 
+    @Override
+    public boolean allowedMove(int prevX, int prevY, int currX, int currY, boolean isEmpty){
+        // change in x
+		int x;
+        //change in y
+		int y;
+		
+		x = Math.abs(currX-prevX);
+		y = Math.abs(currY-prevY);
+
+		if (x==y){
+			return true;
+		}
+        return false;
+    }
+
+    public String toString() {
+		return this.chessPiece;
+	}
+}
