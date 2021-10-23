06e44cb327e19d7c023aac81046766729ad686cd
PAULEENE ADIREA JORDAN <paj81@scarletmail.rutgers.edu>
2021-10-23 18:07:03 +0000

    Knight.java created online with Bitbucket

---
+++ Knight.java
@@ -0,0 +1,41 @@
+package src;
+
+public class Knight extends Piece{
+    
+	private String chessPiece;
+	
+	public Knight(boolean whiteTurn) {
+        super(whiteTurn);
+    
+        this.setWhiteTurn(whiteTurn);
+            //check if white piece is playing or black
+        if(whiteTurn == true)
+        {
+			this.chessPiece = "wN"; 
+		}else{
+			this.chessPiece = "bN"; 
+		}
+	}
+    public boolean allowedMove(int prevX, int prevY, int currX, int currY, boolean isEmpty){
+
+		// change in x
+		int x;
+        //change in y
+		int y;
+		
+		x = Math.abs(currX-prevX);
+		y = Math.abs(currY-prevY);
+		
+		if (x == 2 && y == 1){
+			return true;
+		} else if (x == 1 && y == 2) {
+			return true;
+		}
+				
+		return false;
+	}
+
+	public String toString() {
+		return this.chessPiece;
+	}
+}
